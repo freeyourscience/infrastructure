7dece2b29936b0a6aef15eac2efac39924d4f994
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-09 17:02:16 +0000

    remove bucket used to confirm working tf

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@ provider "google" {
   project     = "stunning-oasis-298115"
   region      = "us-west1"
 }
-
-resource "google_storage_bucket" "testing-tf" {
-  name          = "temp-for-testing"
-  location      = "EU"
-  force_destroy = true
-}
