863fb5fdffd1134a17d7e54e8f7a896f223a5dc3
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-11 16:55:31 +0000

    prep api resources for module migration

--- main.tf
+++ main.tf
@@ -51,12 +51,18 @@ resource "github_actions_secret" "gcp_region" {
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
 
 resource "google_project_service" "gcr" {
   service = "containerregistry.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
--- modules/gcp_apis/main.tf
+++ modules/gcp_apis/main.tf
@@ -1,11 +1,17 @@
 resource "google_project_service" "iam" {
-  service = "iam.googleapis.com"
+  service                    = "iam.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
 
 resource "google_project_service" "gcr" {
-  service = "containerregistry.googleapis.com"
+  service                    = "containerregistry.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
 
 resource "google_project_service" "run" {
-  service = "run.googleapis.com"
+  service                    = "run.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
 }
