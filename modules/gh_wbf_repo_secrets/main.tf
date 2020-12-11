f70df9f27ea157a080456bb21959c71a10629f78
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-11 19:52:42 +0000

    dedupe gh_wbf_repo_secrets using for_each #11

--- modules/gh_wbf_repo_secrets/main.tf
+++ modules/gh_wbf_repo_secrets/main.tf
@@ -4,26 +4,14 @@ data "github_actions_public_key" "example_public_key" {
   repository = "wissenschaftsbefreiungsfront"
 }
 
-resource "github_actions_secret" "sa_key" {
+resource "github_actions_secret" "dev_env" {
+  for_each = {
+    GCP_SA_KEY = var.sa_key
+    GCP_PROJECT_ID = basename(data.google_project.project.id)
+    GCP_CLOUDRUN_SERVICE = "wbf-dev"
+    GCP_REGION = "us-west1"
+  }
   repository      = "wissenschaftsbefreiungsfront"
-  secret_name     = "GCP_SA_KEY"
-  plaintext_value = var.sa_key
-}
-
-resource "github_actions_secret" "gcp_project_id" {
-  repository      = "wissenschaftsbefreiungsfront"
-  secret_name     = "GCP_PROJECT_ID"
-  plaintext_value = basename(data.google_project.project.id)
-}
-
-resource "github_actions_secret" "gcp_cloudrun_service" {
-  repository      = "wissenschaftsbefreiungsfront"
-  secret_name     = "GCP_CLOUDRUN_SERVICE"
-  plaintext_value = "wbf-dev"
-}
-
-resource "github_actions_secret" "gcp_region" {
-  repository      = "wissenschaftsbefreiungsfront"
-  secret_name     = "GCP_REGION"
-  plaintext_value = "us-west1"
+  secret_name     = each.key
+  plaintext_value = each.value
 }
