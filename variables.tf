f9a5d022df743fd208b28f632fc80f93244953d1
Lukas Grossberger <code@grossberger.xyz>
2020-12-15 09:40:38 +0100

    fix s2 api key variable missing

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "sherpa_api_key" {
   description = "API key for v2.sherpa.co.uk"
   sensitive   = true
 }
+
+variable "s2_api_key" {
+  description = "API key for partner.semanticscholar.org/v1"
+  sensitive   = true
+}
