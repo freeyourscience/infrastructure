d559fef56519612b7fbfe3cc065c6cd46cb35538
Daniel Haarhoff <haarhoff@mailbox.org>
2020-12-15 20:44:18 +0000

    Revert "wip: narrowing down error #11"
    This reverts commit 626d001d8174ca716eb96afc9098fd2ddda02ccf.

--- modules/gcp_monitoring/main.tf
+++ modules/gcp_monitoring/main.tf
@@ -71,20 +71,20 @@ resource "google_monitoring_alert_policy" "error_ratio" {
   conditions {
     display_name = "Ratio: 5xx count / total response count"
     condition_threshold {
-      filter = "resource.type=\"cloud_run_revision\" AND metric.type=\"run.googleapis.com/request_count\""
+      filter = "resource.type=\"cloud_run_revision\" AND metric.type=\"run.googleapis.com/request_count\" AND metric.label.service_name=\"wbf-dev\" AND metric.label.response_code_class=\"5xx\""
       aggregations {
         alignment_period     = "300s"
         cross_series_reducer = "REDUCE_SUM"
         per_series_aligner   = "ALIGN_DELTA"
       }
-      denominator_filter = "resource.type=\"cloud_run_revision\" AND metric.type=\"run.googleapis.com/request_count\" "
+      denominator_filter = "resource.type=\"cloud_run_revision\" AND metric.type=\"run.googleapis.com/request_count\" AND metric.label.service_name=\"wbf-dev\" "
       denominator_aggregations {
         alignment_period     = "300s"
         cross_series_reducer = "REDUCE_SUM"
         per_series_aligner   = "ALIGN_DELTA"
       }
       comparison      = "COMPARISON_GT"
-      threshold_value = 1.0
+      threshold_value = 0.0
       duration        = "0s"
     }
   }
