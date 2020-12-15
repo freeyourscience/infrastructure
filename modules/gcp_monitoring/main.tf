3f256130e18fbab3a0584b21188360eec6223c8e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-15 17:06:13 +0000

    add uptime check of paper page #11

--- modules/gcp_monitoring/main.tf
+++ modules/gcp_monitoring/main.tf
@@ -19,3 +19,25 @@ resource "google_monitoring_uptime_check_config" "landingpage_https" {
     }
   }
 }
+
+resource "google_monitoring_uptime_check_config" "paper_api" {
+  display_name = "paperpage-https-uptime-check"
+  timeout      = "20s"
+  period       = "900s"
+
+  http_check {
+    path         = "/papers?doi=+10.7554%2FeLife.07157"
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+
+    labels = {
+      project_id = var.gcp_project
+      host       = "dev.${var.domain_name}"
+    }
+  }
+}
