d551fe76e625b03978640499d96bf67150649967
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-18 11:33:45 +0000

    Revert "Revert "fix: can't have domainmapping without a cloud run deploy route""
    This reverts commit e67b1f49d7432d86e89aeadfae9eba9be2a58cb4.

--- modules/gcp_domainmapping/main.tf
+++ modules/gcp_domainmapping/main.tf
@@ -1,16 +1,3 @@
-resource "google_cloud_run_domain_mapping" "prod" {
-  location = var.gcp_region
-  name     = var.domain_name
-
-  spec {
-    route_name = var.route
-  }
-
-  metadata {
-    namespace = var.gcp_project
-  }
-}
-
 resource "google_cloud_run_domain_mapping" "dev" {
   location = var.gcp_region
   name     = "dev.${var.domain_name}"
