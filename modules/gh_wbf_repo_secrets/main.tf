d5f5779891c24e5b74483843c8e75017b56d9524
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-18 11:39:27 +0000

    enable prod domain mapping
    This reverts commit 0de20679aacd0701044158b99702a204326be514.

--- modules/gh_wbf_repo_secrets/main.tf
+++ modules/gh_wbf_repo_secrets/main.tf
@@ -9,7 +9,7 @@ data "github_actions_public_key" "example_public_key" {
 resource "github_actions_secret" "non_sensitive" {
   for_each = {
     GCP_PROJECT_ID           = var.gcp_project
-    GCP_CLOUDRUN_SERVICE     = var.cloudrun_svc
+    GCP_CLOUDRUN_SERVICE     = var.cloudrun_svc_dev
     GCP_CLOUDRUN_SERVICE_DEV = var.cloudrun_svc_dev
     GCP_REGION               = var.gcp_region
     API_EMAIL                = "team@${var.domain_name}"
